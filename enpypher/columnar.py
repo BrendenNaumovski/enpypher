e095f01d7ca96cf96ccbc1774c4d50cfed2c53e4
Brenden Naumovski <bnaumovski@proton.me>
2025-01-16 22:06:38 -0500

    Implemented Columnar Transposition and added test case boiler plate.

---
+++ enpypher/columnar.py
@@ -0,0 +1,57 @@
+import math
+from typing import List
+
+from enpypher.cipher_machine import CipherMachine
+
+
+class Columnar(CipherMachine):
+    def _cipher(self, text: str, encipher: bool) -> str:
+        grid = self._write_grid(text, encipher)
+        return self._read_grid(grid, encipher)
+
+    def set_key(self, key):
+        self.input_key = key
+        self.clean_key = self._clean_input(key, alpha=self.alpha)
+        sort_key = [(char, i) for i, char in enumerate(sorted(self.clean_key))]
+        og = {char: idx for idx, char in enumerate(self.clean_key)}
+        self.col_map = [
+            idx for char, idx in sorted(sort_key, key=lambda x: og[x[0]])
+        ]
+
+    # Helpers
+    def _write_grid(self, text: str, encipher: bool) -> List[List[str]]:
+        cols = len(self.clean_key)
+        rows = math.ceil(len(text) / cols)
+        last_len = len(text) % cols
+        grid = [[""] * cols for i in range(rows)]
+
+        i = 0
+        if encipher:
+            for row in range(rows):
+                for col in range(cols):
+                    if i < len(text):
+                        grid[row][col] = text[i]
+                        i += 1
+        else:
+            for col in self.col_map:
+                for row in range(rows):
+                    if i < len(text):
+                        if row < rows - 1 or col < last_len:
+                            grid[row][col] = text[i]
+                            i += 1
+
+        return grid
+
+    def _read_grid(self, grid: List[List[str]], encipher: bool) -> str:
+        new_text = []
+
+        if encipher:
+            for col in self.col_map:
+                for row in range(len(grid)):
+                    new_text.append(grid[row][col])
+        else:
+            for row in grid:
+                for char in row:
+                    new_text.append(char)
+
+        return "".join(new_text)
---
+++ tests/test_columnar.py
@@ -0,0 +1,184 @@
+import pytest
+
+from enpypher.columnar import Columnar
+
+
+@pytest.mark.parametrize(
+    "init, pt, ct",
+    [
+        (
+            ["key"],
+            "thequickbrownfoxjumpsoverthelazydog",
+            "SFBPTGYIEQNVMCNWHTLORNUBQSFBJKZXAND",
+        ),
+        (
+            ["KEY"],
+            "thequickbrownfoxjumpsoverthelazydog",
+            "SFBPTGYIEQNVMCNWHTLORNUBQSFBJKZXAND",
+        ),
+        (
+            ["key"],
+            "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG",
+            "SFBPTGYIEQNVMCNWHTLORNUBQSFBJKZXAND",
+        ),
+        (
+            ["key"],
+            "the quick brown fox jumps over the lazy dog",
+            "SFB PTGYI EQNVM CNW HTLOR NUBQ SFB JKZX AND",
+        ),
+        (
+            ["key"],
+            "the quick brown fox jumps over 3 lazy dogs.",
+            "SFB PTGYI EQNVM CNW HTLOR NUBQ 3 JKZX ANDR.",
+        ),
+        (
+            ["key"],
+            "1234567890",
+            "1234567890",
+        ),
+        (
+            ["thequickbrownfoxjumpsoverthelazydog"],
+            "plaintext",
+            "JWTBFVUYV",
+        ),
+        (
+            ["the quick brown fox jumps over 3 lazy dogs."],
+            "plaintext",
+            "JWTBFVUYV",
+        ),
+        (
+            ["secret"],
+            "thequickbrownfoxjumpsoverthelazydog",
+            "QDTNUFCHEOLWKALXGUJMPLVTOQDTISZYRLB",
+        ),
+        (
+            ["defghijklmnopqrstuvwxyzabc"],
+            "thequickbrownfoxjumpsoverthelazydog",
+            "WKHTXLFNEURZQIRAMXPSVRYHUWKHODCBGRJ",
+        ),
+        (
+            ["Ελλάδα", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "Καλημέρα",
+            "ΙΕΚΖΜΒΡΕ",
+        ),
+        (
+            ["ΜΥΣΤΙΚΟ", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "Η γρήγορη καφετιά αλεπού πηδά πάνω από το τεμπέλικο σκυλί.",
+            "Ο ΣΝΟΣΘΝΟ ΓΜΦΙΠΒΜ ΜΔΙΛΘΡ ΛΟΤΜ ΛΜΖΩ ΜΛΘ ΠΘ ΠΙΕΛΙΔΒΓΘ ΞΓΡΔΒ.",
+        ),
+    ],
+)
+def test_encipher(init, pt, ct):
+    c = Columnar(*init)
+    assert c.encipher(pt) == ct
+
+
+@pytest.mark.parametrize(
+    "init, ct, pt",
+    [
+        (
+            ["key"],
+            "SFBPTGYIEQNVMCNWHTLORNUBQSFBJKZXAND",
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["KEY"],
+            "SFBPTGYIEQNVMCNWHTLORNUBQSFBJKZXAND",
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["key"],
+            "sfbptgyieqnvmcnwhtlornubqsfbjkzxand",
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["key"],
+            "SFB PTGYI EQNVM CNW HTLOR NUBQ SFB JKZX AND",
+            "the quick brown fox jumps over the lazy dog",
+        ),
+        (
+            ["key"],
+            "SFB PTGYI EQNVM CNW HTLOR NUBQ 3 JKZX ANDR.",
+            "the quick brown fox jumps over 3 lazy dogs.",
+        ),
+        (
+            ["key"],
+            "1234567890",
+            "1234567890",
+        ),
+        (
+            ["thequickbrownfoxjumpsoverthelazydog"],
+            "JWTBFVUYV",
+            "plaintext",
+        ),
+        (
+            ["the quick brown fox jumps over 3 lazy dogs."],
+            "JWTBFVUYV",
+            "plaintext",
+        ),
+        (
+            ["secret"],
+            "QDTNUFCHEOLWKALXGUJMPLVTOQDTISZYRLB",
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["defghijklmnopqrstuvwxyzabc"],
+            "WKHTXLFNEURZQIRAMXPSVRYHUWKHODCBGRJ",
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["Ελλάδα", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "ΙΕΚΖΜΒΡΕ",
+            "καλημερα",
+        ),
+        (
+            ["ΜΥΣΤΙΚΟ", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "Ο ΣΝΟΣΘΝΟ ΓΜΦΙΠΒΜ ΜΔΙΛΘΡ ΛΟΤΜ ΛΜΖΩ ΜΛΘ ΠΘ ΠΙΕΛΙΔΒΓΘ ΞΓΡΔΒ.",
+            "η γρηγορη καφετια αλεπου πηδα πανω απο το τεμπελικο σκυλι.",
+        ),
+    ],
+)
+def test_decipher(init, ct, pt):
+    c = Columnar(*init)
+    assert c.decipher(ct) == pt
+
+
+@pytest.mark.parametrize(
+    "init, exp_key",
+    [
+        (["key"], "key"),
+        (["KEY"], "KEY"),
+        (["secret"], "secret"),
+        (["defghijklmnopqrstuvwxyzabc"], "defghijklmnopqrstuvwxyzabc"),
+        (
+            ["thequickbrownfoxjumpsoverthelazydog"],
+            "thequickbrownfoxjumpsoverthelazydog",
+        ),
+        (
+            ["the quick brown fox jumps over 3 lazy dogs."],
+            "the quick brown fox jumps over 3 lazy dogs.",
+        ),
+        (
+            ["ΜΥΣΤΙΚΟ", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "ΜΥΣΤΙΚΟ",
+        ),
+    ],
+)
+def test_key(init, exp_key):
+    c = Columnar(*init)
+    assert c.key() == exp_key
+
+
+@pytest.mark.parametrize(
+    "init, exp_alpha",
+    [
+        (["key"], "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+        (
+            ["ΜΥΣΤΙΚΟ", "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"],
+            "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ",
+        ),
+    ],
+)
+def test_alphabet(init, exp_alpha):
+    c = Columnar(*init)
+    assert c.alphabet() == exp_alpha
